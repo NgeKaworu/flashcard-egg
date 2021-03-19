f7e122aeaac62e085f8d9ca061f6104b44ab9fcf
fuRan <NgeKaworu@gmail.com>
2021-03-19 21:59:41 +0800

    ts

--- app/controller/record.ts
+++ app/controller/record.ts
@@ -247,7 +247,7 @@ export default class RecordController extends Controller {
       ]);
 
       const random = await cursor.toArray();
-      const ids = random.map((i) => new ObjectID(i?._id));
+      const ids = random.map(i => new ObjectID(i?._id));
 
       const filter = {
         uid: new ObjectID(ctx.uid),
